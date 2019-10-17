2929507d2140d29c8aeff23f59d9934b3ad1e0d9
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-17 15:58:30 +0200

    🚧 login

--- apps/websheep/src/app/login/login.component.ts
+++ apps/websheep/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexModule } from '@angular/flex-layout';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatInputModule } from '@angular/material';
 
 @Component({
   selector: 'ws-login',
@@ -7,14 +10,27 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    userName: new FormControl(),
+    password: new FormControl()
+  });
+
   constructor() {}
 
   ngOnInit() {}
+
+  logIn() {}
 }
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    FlexModule
+  ],
   exports: [LoginComponent]
 })
 export class LoginModule {}
