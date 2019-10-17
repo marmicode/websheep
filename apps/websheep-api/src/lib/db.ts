128eb95b4d0c27e056d43567b522352c5d03eb1d
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-17 18:12:51 +0200

    🚧 login

--- apps/websheep-api/src/lib/db.ts
+++ apps/websheep-api/src/lib/db.ts
@@ -4,5 +4,9 @@ import * as FileSync from 'lowdb/adapters/FileSync';
 export const database = low(new FileSync('db.json'));
 
 export function initializeDatabase() {
-  database.defaults({}).write();
+  database
+    .defaults({
+      farmers: require('../fixtures/farmers.json')
+    })
+    .write();
 }
