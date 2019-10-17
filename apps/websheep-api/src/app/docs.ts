944399f5a1075fcbafd8f29a7fd9ff3b269df268
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-17 14:43:41 +0200

    ✅ host openapi yaml and json files

--- apps/websheep-api/src/app/docs.ts
+++ apps/websheep-api/src/app/docs.ts
@@ -4,7 +4,26 @@ import * as yaml from 'yamljs';
 
 export const docsRouter = Router();
 
-const swaggerDocument = yaml.parse(require('./websheep.yaml').default);
+const swaggerRaw = require('./websheep.yaml').default;
+const swaggerDocument = yaml.parse(swaggerRaw);
 
 docsRouter.use('', swaggerUi.serve);
-docsRouter.get('', swaggerUi.setup(swaggerDocument));
+docsRouter.get(
+  '',
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      urls: [
+        {
+          name: 'YAML Specification',
+          url: '/docs/specification.yaml'
+        },
+        {
+          name: 'JSON Specification',
+          url: '/docs/specification.json'
+        }
+      ]
+    }
+  })
+);
+docsRouter.get('/specification.yaml', (req, res) => res.send(swaggerRaw));
+docsRouter.get('/specification.json', (req, res) => res.send(swaggerDocument));
