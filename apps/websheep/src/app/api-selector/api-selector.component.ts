0d8090ee0cc2eb0c36c9dd56463b8a95af95d1b0
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-17 17:07:33 +0200

    🚧 add api selector

--- apps/websheep/src/app/api-selector/api-selector.component.ts
+++ apps/websheep/src/app/api-selector/api-selector.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule, OnInit } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material';
 import { Store } from '@ngrx/store';
 import * as fromConfig from '../config/config.selectors';
 import { AppState } from '../reducers';
@@ -11,15 +13,20 @@ import { AppState } from '../reducers';
 })
 export class ApiSelectorComponent implements OnInit {
   apiBaseUrl$ = this._store.select(fromConfig.apiBaseUrl);
+  isEditing = false;
 
   constructor(private _store: Store<AppState>) {}
 
   ngOnInit() {}
+
+  edit() {
+    this.isEditing = true;
+  }
 }
 
 @NgModule({
   declarations: [ApiSelectorComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, FlexModule],
   exports: [ApiSelectorComponent]
 })
 export class ApiSelectorModule {}
