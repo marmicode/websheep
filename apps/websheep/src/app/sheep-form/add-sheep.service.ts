442e5871155bec7b773fa4b131b9847a06d16863
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-18 19:25:05 +0200

    ✅ redirect to sheeps list after adding sheep

--- apps/websheep/src/app/sheep-form/add-sheep.service.ts
+++ apps/websheep/src/app/sheep-form/add-sheep.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 import { Sheep } from '../sheep-core/sheep';
+import { sheepRouteHelper } from '../views/sheep/sheep-route-helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddSheepService {
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private _httpClient: HttpClient, private _router: Router) {}
 
   addSheep({ sheep }: { sheep: Sheep }) {
-    return this._httpClient.post<Sheep>('/sheep', sheep);
+    return this._httpClient
+      .post<Sheep>('/sheep', sheep)
+      .pipe(
+        tap(() => this._router.navigate(sheepRouteHelper.sheepListRoute()))
+      );
   }
 }
