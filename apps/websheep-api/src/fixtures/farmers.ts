37d313836b623dea07e2175486d243f51cb588cb
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-18 16:43:59 +0200

    🚧 add /farmers/:farmerId route

--- apps/websheep-api/src/fixtures/farmers.ts
+++ apps/websheep-api/src/fixtures/farmers.ts
@@ -4,20 +4,23 @@ export const farmers = [
     firstName: 'Karine',
     lastName: 'Le Marchand',
     /* 123456 */
-    passwordHash: 'zmkO/W+ZrHp015/8LbDoQ/3DrRIYagekcf8icPqSBVk='
+    passwordHash: 'zmkO/W+ZrHp015/8LbDoQ/3DrRIYagekcf8icPqSBVk=',
+    isAdmin: false
   },
   {
     id: 'foobar',
     firstName: 'foo',
     lastName: 'Bar',
     /* zTAr@6y60PwJ */
-    passwordHash: 'cm+Gzc9HqStHBB7E92lRZ7jDmuwAPfTQUvrQstGCbvs='
+    passwordHash: 'cm+Gzc9HqStHBB7E92lRZ7jDmuwAPfTQUvrQstGCbvs=',
+    isAdmin: false
   },
   {
     id: 'johndoe',
     firstName: 'John',
     lastName: 'DOE',
     /* %yP50h^qI02t */
-    passwordHash: '494yx+2TfffQcs1c5xg4BQB8ubAd6bPK56e/eX4mTf0='
+    passwordHash: '494yx+2TfffQcs1c5xg4BQB8ubAd6bPK56e/eX4mTf0=',
+    isAdmin: false
   }
 ];
