8eadcb34fd86cccb263b8efc5e8ac67208b5936e
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-19 14:33:35 +0200

    🚧 add hack assistant

--- apps/websheep/src/app/hack-menu/hack-assistant.component.ts
+++ apps/websheep/src/app/hack-menu/hack-assistant.component.ts
@@ -1,15 +1,30 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+export interface Mission {
+  title: string;
+  topic: string;
+  goals?: string[];
+  hints?: string[];
+  // @todo config: Config
+}
+
 @Component({
   selector: 'ws-hack-assistant',
   templateUrl: './hack-assistant.component.html',
   styleUrls: ['./hack-assistant.component.scss']
 })
-export class HackAssistantComponent implements OnInit {
-  constructor() {}
+export class HackAssistantComponent {
+  topics = ['Authorization'];
 
-  ngOnInit() {}
+  missions: Mission[] = [
+    {
+      title: 'Catch a sheep herd',
+      topic: 'Authorization',
+      goals: [`Grab the names of Foo Bar's sheep`],
+      hints: [`Foo Bar's user id is "foobar"`]
+    }
+  ];
 }
 
 @NgModule({
