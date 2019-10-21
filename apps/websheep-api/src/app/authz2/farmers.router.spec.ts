f8c53e0ae35a7bf31a9b3db25ea56ee33a4488ac
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-21 19:33:37 +0200

    🚧 add csrf mission with permissive cors rules

---
+++ apps/websheep-api/src/app/authz2/farmers.router.spec.ts
@@ -0,0 +1,48 @@
+import * as bodyParser from 'body-parser';
+import { Express } from 'express';
+import * as request from 'supertest';
+import * as express from 'express';
+import { initializeDatabase } from '../database';
+import { farmersRouter } from './farmers.router';
+import { authz2Router } from './index';
+
+describe('sheep router', () => {
+  let app: Express;
+
+  beforeEach(() => {
+    app = express();
+
+    app.use(bodyParser.json());
+
+    /* Suppose user is authenticated. */
+    app.use((req, res, next) => {
+      req['user'] = {
+        id: 'karinelemarchand'
+      };
+      next();
+    });
+
+    app.use(farmersRouter);
+
+    initializeDatabase();
+  });
+
+  it(`should allow farmer to escalate to admin`, async () => {
+    /* Escalate to isAdmin. */
+    const patchResult = await request(app)
+      .patch('/farmers/karinelemarchand')
+      .send({
+        isAdmin: true
+      });
+    expect(patchResult.status).toEqual(200);
+
+    /* Make sure isAdmin is now true. */
+    const getResult = await request(app).get('/farmers/karinelemarchand');
+    expect(getResult.status).toEqual(200);
+    expect(getResult.body).toEqual(
+      expect.objectContaining({
+        isAdmin: true
+      })
+    );
+  });
+});
