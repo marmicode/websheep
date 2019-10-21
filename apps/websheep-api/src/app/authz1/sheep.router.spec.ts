8cdee74c9a986455711c029a2a51200d30b0ac11
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-21 19:12:16 +0200

    🚧 add csrf mission with permissive cors rules

---
+++ apps/websheep-api/src/app/authz1/sheep.router.spec.ts
@@ -0,0 +1,21 @@
+import { Express } from 'express';
+import * as request from 'supertest';
+import * as express from 'express';
+import { initializeDatabase } from '../database';
+import { sheepRouter } from './sheep.router';
+
+describe('sheep router', () => {
+  let app: Express;
+
+  beforeEach(() => {
+    app = express();
+    app.use(sheepRouter);
+    initializeDatabase();
+  });
+
+  it(`should get farmer's sheep without authorization`, async () => {
+    const { body } = await request(app).get('/farmers/foobar/sheep');
+    expect(body.totalCount).toEqual(13);
+    expect(body.items[0].name).toEqual('Adriana');
+  });
+});
