618de364f50ccb6a05f1ecd7a7edf9e6547e767c
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-22 22:29:12 +0200

    🚧 setup deployment on zeit now

--- apps/websheep-api/src/app/database.ts
+++ apps/websheep-api/src/app/database.ts
@@ -4,7 +4,10 @@ import { farmers } from '../fixtures/farmers';
 import { farms } from '../fixtures/farms';
 import { sheep } from '../fixtures/sheep';
 
-export const database = low(new FileSync('db.json'));
+export const databaseFilePath =
+  process.env.NODE_ENV === 'production' ? '/tmp/db.json' : 'db.json';
+
+export const database = low(new FileSync(databaseFilePath));
 
 export function initializeDatabase() {
   database
