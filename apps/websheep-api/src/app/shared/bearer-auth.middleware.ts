c3d51ee3d0303260cf81a34f5fc7d9eaf8370a35
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-23 21:23:56 +0200

    🚧 add jwt mission ignoring token signature

--- apps/websheep-api/src/app/shared/bearer-auth.middleware.ts
+++ apps/websheep-api/src/app/shared/bearer-auth.middleware.ts
@@ -1,15 +1,14 @@
 import { Passport } from 'passport';
 import { Strategy as BearerStrategy } from 'passport-http-bearer';
+import { callbackify } from 'util';
 import { farmersService } from './farmer/farmers.service';
 
 const passport = new Passport();
 
 passport.use(
-  new BearerStrategy((token, done) => {
-    const farmer = farmersService.getByToken({ token });
-
-    farmer ? done(null, farmer) : done(null, false);
-  })
+  new BearerStrategy(
+    callbackify(async token => farmersService.getByToken({ token }))
+  )
 );
 
 export const bearerAuthMiddleware = passport.authenticate('bearer', {
