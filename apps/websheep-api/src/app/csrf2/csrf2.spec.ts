391bd8ae84f923f28ec7f928ca9f54955851a71b
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-30 20:50:07 +0100

    🛠 add test for csrf2

---
+++ apps/websheep-api/src/app/csrf2/csrf2.spec.ts
@@ -0,0 +1,51 @@
+import * as express from 'express';
+import { Express } from 'express';
+import * as request from 'supertest';
+import { initializeDatabase } from '../database';
+import { farmersService } from '../shared/farmer/farmers.service';
+import { openApiValidator } from '../shared/openapi/validator';
+import { csrf2Router } from './index';
+
+describe('tokens router', () => {
+  let app: Express;
+  const validateSheepResponse = openApiValidator.validateResponse(
+    'post',
+    '/sheep'
+  );
+
+  beforeEach(() => {
+    app = express();
+
+    farmersService.getByToken = jest.fn().mockReturnValue({
+      id: 'USER_ID'
+    });
+
+    app.use(csrf2Router);
+
+    initializeDatabase();
+  });
+
+  it(`should allow x-www-form-urlencoded`, async () => {
+    const response = await request(app)
+      .post('/sheep')
+      .set('Cookie', ['token=TOKEN_VALUE'])
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send('name=Dolly&farm[id]=FARM_ID');
+
+    expect(response.status).toEqual(201);
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        name: 'Dolly'
+      })
+    );
+
+    /* ACAO header should be set to default allowed origin. */
+    expect(response.headers['access-control-allow-origin']).toEqual(
+      'http://localhost:4200'
+    );
+
+    /* Validate schema at the end cuz openApiValidator seems to mutate the response. */
+    expect(() => validateSheepResponse(response)).not.toThrowError();
+  });
+});
