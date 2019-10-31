ae53b1798cd8e9d3f4e3372e9d66ec7169c06630
Younes JAAIDI <yjaaidi@gmail.com>
2019-10-31 11:49:51 +0100

    ✅ add link to openapi specification on toolbar

--- apps/websheep/src/app/app.component.ts
+++ apps/websheep/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Signout } from './auth/signout';
+import { getApiBaseUrl } from './config/config.selectors';
 import { FarmerService } from './farmer/farmer.service';
 import { hideHackAssistant, showHackAssistant } from './layout/layout.actions';
 import { getIsHackAssistantOpen } from './layout/layout.selectors';
@@ -17,8 +18,11 @@ import { sheepRouteHelper } from './views/sheep/sheep-route-helper';
 export class AppComponent {
   currentFarmer$ = this._farmerService.currentFarmer$;
   isHackAssistantOpen$ = this._store.select(getIsHackAssistantOpen);
+  apiBaseUrl$ = this._store.select(getApiBaseUrl);
   sheepRouteHelper = sheepRouteHelper;
   greetings$: Observable<string>;
+
+  swaggerLogoUri = require('./swagger-logo.png');
 
   constructor(
     private _farmerService: FarmerService,
