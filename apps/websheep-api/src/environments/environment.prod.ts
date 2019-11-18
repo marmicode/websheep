1cf81b585072d86124251f284c7e1bdb7f868234
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-18 09:29:16 +0100

    ✅ add https://websheep.now.sh to whitelisted origins

--- apps/websheep-api/src/environments/environment.prod.ts
+++ apps/websheep-api/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiServerUrl: 'https://websheep.herokuapp.com',
-  appOrigin: 'https://websheep.yjaaidi.now.sh',
+  appOrigin: ['https://websheep.yjaaidi.now.sh', 'https://websheep.now.sh'],
   production: true
 };
