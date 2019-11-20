34c7ff3e092f6039d82786776db8969391ab607f
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-20 09:08:11 +0100

    fix: 🐞 use `farmerId` from params

--- apps/websheep-api/src/app/shared/farmer/get-farmer.ts
+++ apps/websheep-api/src/app/shared/farmer/get-farmer.ts
@@ -2,6 +2,6 @@ import { farmersService } from './farmers.service';
 import { serializeFarmer } from './serialize-farmer';
 
 export function getFarmer(req, res) {
-  const farmerId = req.user.id;
+  const farmerId = req.params.farmerId;
   res.json(serializeFarmer(farmersService.getFarmer({ farmerId })));
 }
