6e97cb890112f0786823ff7b674535d8bf708c12
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-21 17:06:28 +0100

    🛠 move pact interaction test logic to dedicated function

--- apps/websheep-api/src/app/authz1/pact.spec.ts
+++ apps/websheep-api/src/app/authz1/pact.spec.ts
@@ -1,13 +1,12 @@
 import * as express from 'express';
 import { Express } from 'express';
-import * as jp from 'jsonpath';
-import * as request from 'supertest';
 import { authz1Router } from '.';
 import { interactions } from '../../../../../libs/pacts/websheep-websheepapi.json';
 import { resetDatabase } from './../database';
 import { farmsService } from './../shared/farm/farms.service';
 import { farmersService } from './../shared/farmer/farmers.service';
 import { sheepService } from './../shared/sheep/sheep.service';
+import { testPactInteraction } from '../shared/test-pact-interaction';
 
 describe('websheep api contract testing', () => {
   let app: Express;
@@ -47,53 +46,6 @@ describe('websheep api contract testing', () => {
 
     await stateProviders[state]();
 
-    const res = await request(app)
-      [interaction.request.method.toLowerCase()](interaction.request.path)
-      .set(interaction.request.headers);
-
-    /* Use simple object type for jsonpath to work. */
-    const response = {
-      body: res.body,
-      status: res.status,
-      headers: res.headers
-    };
-
-    expect(response.headers).toEqual(
-      expect.objectContaining(interaction.response.headers)
-    );
-
-    jp.nodes(interaction.response.body, '$..*')
-      /* Ignore objects and keep leaves. */
-      .filter(({ value }) => typeof value !== 'object')
-      .forEach(({ path, value }) => {
-        /* Add `body` level. */
-        path = ['$', 'body', ...path.slice(1)];
-
-        const pathStr = path.join('.').replace(/\.(\d+)/g, '[$1]');
-        const matchingRule = interaction.response.matchingRules[pathStr];
-
-        switch (matchingRule ? matchingRule.match : null) {
-          case 'type':
-            expect({
-              /* Add path for logs. */
-              path: pathStr,
-              value: typeof jp.query(response, pathStr)[0]
-            }).toEqual({ path: pathStr, value: typeof value });
-            break;
-          case 'regex':
-              expect({
-                /* Add path for logs. */
-                path: pathStr,
-                value: value.match(matchingRule.regex) != null
-              }).toEqual({ path: pathStr, value: true });
-            break;
-          default:
-            expect({
-              /* Add path for logs. */
-              path: pathStr,
-              value: jp.query(response, pathStr)[0]
-            }).toEqual({ path: pathStr, value });
-        }
-      });
+    await testPactInteraction({ app, interaction });
   });
 });
---
+++ apps/websheep-api/src/app/shared/test-pact-interaction.ts
@@ -0,0 +1,55 @@
+import { Express } from 'express';
+import * as jp from 'jsonpath';
+import * as request from 'supertest';
+
+export async function testPactInteraction({
+  app,
+  interaction
+}: {
+  app: Express;
+  interaction;
+}) {
+  const res = await request(app)
+    [interaction.request.method.toLowerCase()](interaction.request.path)
+    .set(interaction.request.headers);
+  /* Use simple object type for jsonpath to work. */
+  const response = {
+    body: res.body,
+    status: res.status,
+    headers: res.headers
+  };
+  expect(response.headers).toEqual(
+    expect.objectContaining(interaction.response.headers)
+  );
+  jp.nodes(interaction.response.body, '$..*')
+    /* Ignore objects and keep leaves. */
+    .filter(({ value }) => typeof value !== 'object')
+    .forEach(({ path, value }) => {
+      /* Add `body` level. */
+      path = ['$', 'body', ...path.slice(1)];
+      const pathStr = path.join('.').replace(/\.(\d+)/g, '[$1]');
+      const matchingRule = interaction.response.matchingRules[pathStr];
+      switch (matchingRule ? matchingRule.match : null) {
+        case 'type':
+          expect({
+            /* Add path for logs. */
+            path: pathStr,
+            value: typeof jp.query(response, pathStr)[0]
+          }).toEqual({ path: pathStr, value: typeof value });
+          break;
+        case 'regex':
+          expect({
+            /* Add path for logs. */
+            path: pathStr,
+            value: value.match(matchingRule.regex) != null
+          }).toEqual({ path: pathStr, value: true });
+          break;
+        default:
+          expect({
+            /* Add path for logs. */
+            path: pathStr,
+            value: jp.query(response, pathStr)[0]
+          }).toEqual({ path: pathStr, value });
+      }
+    });
+}
